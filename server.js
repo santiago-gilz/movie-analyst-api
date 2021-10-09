8a8b2ecfae8a82708c311f5b05e30d11bc7c0a03
Santiago Gil <santiago.gil@loanlogics.com>
2021-10-09 18:23:46 -0500

    Done with db integration

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var mysql = require("mysql");
 var connection = mysql.createConnection({
- host     : process.env.DB_HOST || 'mysql-test.cxrpknmq0hfi.us-west-2.rds.amazonaws.com',
+ host     : process.env.DB_HOST || 'localhost',
  user     : process.env.DB_USER || 'root',
  password : process.env.DB_PASS || '',
  database : process.env.DB_NAME || 'movie_db'
@@ -11,13 +11,12 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-function getTable(callback, table) {    
-       connection.query("SELECT * FROM movie_db.${table}",
-          function (err, rows) {
-            if (err) throw err;
-            res.json(rows);
-          }
-       );    
+function getTable(table, callback) {
+  connection.query(`SELECT * FROM movie_db.${table}`,
+    (err, rows) => {
+      callback(err, rows); 
+    }
+  );    
 }
 
 //Testing endpoint
@@ -37,37 +36,46 @@ app.get('/movies', function(req, res){
   //   {title : 'Ant-Man', release: '2015', score: 8, reviewer: 'Martin Thomas', publication : 'TheOne'},
   //   {title : 'Guardians of the Galaxy', release : '2014', score: 10, reviewer: 'Anthony Miller', publication : 'ComicBookHero.com'},
   // ]
-  getTable("movies");
+  getTable("moviereview", (err, dbResult) => {
+    if (err) throw err;
+    res.json(dbResult);
+  });
 })
 
 // Implement the reviewers API endpoint
 app.get('/reviewers', function(req, res){
-  var authors = [
-    {name : 'Robert Smith', publication : 'The Daily Reviewer', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/angelcolberg/128.jpg'},
-    {name: 'Chris Harris', publication : 'International Movie Critic', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/bungiwan/128.jpg'},
-    {name: 'Janet Garcia', publication : 'MoviesNow', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/grrr_nl/128.jpg'},
-    {name: 'Andrew West', publication : 'MyNextReview', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/d00maz/128.jpg'},
-    {name: 'Mindy Lee', publication: 'Movies n\' Games', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/laurengray/128.jpg'},
-    {name: 'Martin Thomas', publication : 'TheOne', avatar : 'https://s3.amazonaws.com/uifaces/faces/twitter/karsh/128.jpg'},
-    {name: 'Anthony Miller', publication : 'ComicBookHero.com', avatar : 'https://s3.amazonaws.com/uifaces/faces/twitter/9lessons/128.jpg'}
-  ];
+  // var authors = [
+  //   {name : 'Robert Smith', publication : 'The Daily Reviewer', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/angelcolberg/128.jpg'},
+  //   {name: 'Chris Harris', publication : 'International Movie Critic', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/bungiwan/128.jpg'},
+  //   {name: 'Janet Garcia', publication : 'MoviesNow', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/grrr_nl/128.jpg'},
+  //   {name: 'Andrew West', publication : 'MyNextReview', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/d00maz/128.jpg'},
+  //   {name: 'Mindy Lee', publication: 'Movies n\' Games', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/laurengray/128.jpg'},
+  //   {name: 'Martin Thomas', publication : 'TheOne', avatar : 'https://s3.amazonaws.com/uifaces/faces/twitter/karsh/128.jpg'},
+  //   {name: 'Anthony Miller', publication : 'ComicBookHero.com', avatar : 'https://s3.amazonaws.com/uifaces/faces/twitter/9lessons/128.jpg'}
+  // ];
 
-  res.json(authors);
+  getTable("reviewer", (err, dbResult) => {
+    if (err) throw err;
+    res.json(dbResult);
+  });
 })
 
 // Implement the publications API endpoint
 app.get('/publications', function(req, res){
-  var publications = [
-    {name : 'The Daily Reviewer', avatar: 'glyphicon-eye-open'},
-    {name : 'International Movie Critic', avatar: 'glyphicon-fire'},
-    {name : 'MoviesNow', avatar: 'glyphicon-time'},
-    {name : 'MyNextReview', avatar: 'glyphicon-record'},
-    {name : 'Movies n\' Games', avatar: 'glyphicon-heart-empty'},
-    {name : 'TheOne', avatar : 'glyphicon-globe'},
-    {name : 'ComicBookHero.com', avatar : 'glyphicon-flash'}
-  ];
+  // var publications = [
+  //   {name : 'The Daily Reviewer', avatar: 'glyphicon-eye-open'},
+  //   {name : 'International Movie Critic', avatar: 'glyphicon-fire'},
+  //   {name : 'MoviesNow', avatar: 'glyphicon-time'},
+  //   {name : 'MyNextReview', avatar: 'glyphicon-record'},
+  //   {name : 'Movies n\' Games', avatar: 'glyphicon-heart-empty'},
+  //   {name : 'TheOne', avatar : 'glyphicon-globe'},
+  //   {name : 'ComicBookHero.com', avatar : 'glyphicon-flash'}
+  // ];
 
-  res.json(publications);
+  getTable("publication", (err, dbResult) => {
+    if (err) throw err;
+    res.json(dbResult);
+  });
 })
 
 // Implement the pending reviews API endpoint
